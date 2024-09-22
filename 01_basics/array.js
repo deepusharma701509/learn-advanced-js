ccb9756271e49005f3b36d50150c09cb8f52b75d
deepusharma701509 <123170251+deepusharma701509@users.noreply.github.com>
2024-09-22 14:59:01 +0000

    Basics of Js

---
+++ 01_basics/array.js
@@ -0,0 +1,41 @@
+// console.log("Hello Array");
+
+const myArr=[1,3,4,3,6,8];
+
+// console.log(myArr);
+
+// Array Methods
+// myArr.push(60);
+// console.log(myArr);
+
+// myArr.pop()
+// // console.log(myArr);
+
+// const newArray=myArr.join();
+// console.log( myArr);
+
+// console.log(typeof myArr);
+// console.log(typeof newArray);
+// console.log( newArray);
+
+console.log("A", myArr);
+
+const myN1=myArr.slice(1,3)
+console.log(myN1);
+
+console.log("B", myArr);
+
+const myN2=myArr.splice(1,3);
+console.log("C", myArr);
+
+console.log(myN2);
+
+//  slice it creates the copy of the data and perform the operation on that;
+// splice it applies on the original data and manupulates thae original array
+
+
+
+
+
+
+
--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,34 @@
-console.log("Hello Web")
+console.log("Hello Web");
+// 
+
+// let myDate=new Date();
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toLocaleDateString);
+// console.log(myDate.toTimeString());
+// console.log(typeof myDate);
+
+// let myCreatedDate=new Date(2023,0,23);
+
+// console.log(myCreatedDate.toDateString());
+
+// let myTimeStamp=new Date();
+// console.log(myTimeStamp);
+// console.log(Math.floor(myTimeStamp.getTime()/1000));
+
+
+// let newDate=new Date();
+// console.log(newDate);
+// console.log(newDate.getMonth());
+// console.log(newDate.getDate());
+// console.log(newDate.getFullYear());
+
+// console.log(newDate.toLocaleString("default",{
+//     weekday:"long",
+//     month:"2-digit"
+// }));
+
+
+
+
